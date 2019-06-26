7a9699beab43c65d7f60d8082b549c455b11585a
bl419cam <bopingliu88@gmail.com>
2019-06-26 16:49:44 -0700

    correct formatting to pep8

--- moving_averages.py
+++ moving_averages.py
@@ -3,23 +3,25 @@ from typing import List, Dict
 J = [4, 4, 4, 9, 10, 11, 12]
 p = 3
 
+
 def gen_seq(j_list: List[int], p: int) -> Dict[float, float]:
-    """Generates sequence of tuples from a list 
+    """Generates sequence of tuples from a list
     and calculates the min-max averages
     """
-    sequences =[]
+    sequences = []
     output = {}
 
     for ind, _ in enumerate(j_list):
         seq = j_list[ind:ind+p]
         if len(seq) == p:
             sequences.append(seq)
-    
+
     avgs = [sum(sq)/len(sq) for sq in sequences]
 
     output['min'] = min(avgs)
     output['max'] = max(avgs)
 
     return output
+
 
 print(gen_seq(J, p))
